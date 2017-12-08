291e4443c66afbf68ffdb08384e08d1f16f7365e
sharminex81 <mimetyshanta@gmail.com>
2017-12-09 02:29:30 +0600

    UUID generator class has added

---
+++ src/Unique/UUID.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Write something about the purpose of this file
+ *
+ * @author Sharmin Shanta <shantaex81@gmail.com>
+ * @url https://www.sharminshanta.com
+ */
+
+namespace SharminShanta\PHPUtilities\Unique;
+
+/**
+ * Class UUID
+ * @package SharminShanta\PHPUtilities\Unique
+ */
+class UUID
+{
+    /**
+     * @return string
+     */
+    public static function generateUUID()
+    {
+        return sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            // 32 bits for "time_low"
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            // 16 bits for "time_mid"
+            mt_rand(0, 0xffff),
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand(0, 0x0fff) | 0x4000,
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand(0, 0x3fff) | 0x8000,
+            // 48 bits for "node"
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff)
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public static function code8()
+    {
+        return sprintf(
+            '%04x%04x',
+            // 32 bits for "time_low"
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            // 16 bits for "time_mid"
+            mt_rand(0, 0xffff),
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand(0, 0x0fff) | 0x4000,
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand(0, 0x3fff) | 0x8000,
+            // 48 bits for "node"
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff)
+        );
+    }
+}
